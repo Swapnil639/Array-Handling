e20b81807c1b3eebb13f15d70cfd9707d35c9f8a
Swapnil639 <swapnilpatil631997@gmail.com>
2023-03-30 17:09:26 +0530

    Added :Print element of odd position

---
+++ OddPosition.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class OddPosition {
+    public static void main(String[] args) {
+        int[] a={10,30,45,67,89,39,28,1};
+        for (int i = 0; i <a.length; i++) {
+            if (i%2 !=0){
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
