ef127a12e75c457da0034d5db7a2f152fcc33422
Swapnil639 <swapnilpatil631997@gmail.com>
2023-03-30 17:02:51 +0530

    Added :Print element of an array

---
+++ PrintElement.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class PrintElement {
+    public static void main(String[] args) {
+        int[] a={10,30,45,67,89,39,28,19};
+        for (int i = 0; i <a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
